9ec66d4fc7d3cb6f1e2cf639f5391d37cc374b4e
WarperSan <leumas.ecole@gmail.com>
2025-12-11 13:08:40 -0500

    Added `CommandApp` to main

--- ThunderPipe/Program.cs
+++ ThunderPipe/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Spectre.Console.Cli;
+using ThunderPipe.Commands;
+
+namespace ThunderPipe;
+
+internal static class Program
+{
+	public static int Main(string[] args)
+	{
+		var app = new CommandApp();
+
+		app.Configure(config =>
+		{
+			config.AddCommand<PublishCommand>("publish")
+			      .WithDescription("Publish a package to the Thunderstore servers.");
+		});
+
+		return app.Run(args);
+	}
+}
