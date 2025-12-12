284ac72d250d5ad6d6ba12d0d2dec27262fd67f5
WarperSan <leumas.ecole@gmail.com>
2025-12-12 14:20:53 -0500

    Added other method for more flexibility

--- ThunderPipe/Utils/ThunderstoreClient.cs
+++ ThunderPipe/Utils/ThunderstoreClient.cs
@@ -11,15 +11,24 @@ internal sealed class ThunderstoreClient : HttpClient
 	}
 
 	/// <summary>
-	/// Sends the given request, and returns the given JSON response
+	/// Sends the given request
 	/// </summary>
-	public static async Task<T?> SendRequest<T>(HttpRequestMessage request, CancellationToken cancellationToken)
+	public static async Task<HttpResponseMessage> SendRequest(HttpRequestMessage request, CancellationToken cancellationToken)
 	{
 		using var client = new ThunderstoreClient();
 		
 		var response = await client.SendAsync(request, cancellationToken);
 		response.EnsureSuccessStatusCode();
 
+		return response;
+	}
+
+	/// <summary>
+	/// Sends the given request, and returns the given JSON response
+	/// </summary>
+	public static async Task<T?> SendRequest<T>(HttpRequestMessage request, CancellationToken cancellationToken)
+	{
+		var response = await SendRequest(request, cancellationToken);
 		var content = await response.Content.ReadAsStringAsync(cancellationToken);
 
 		return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(content);
