995cc20e7ac8129966c2f2054c697f3653f8f755
WarperSan <leumas.ecole@gmail.com>
2025-12-16 23:07:26 -0500

    Added icon

--- ThunderPipe/Commands/ValidateCommand.cs
+++ ThunderPipe/Commands/ValidateCommand.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
+using Spectre.Console;
 using Spectre.Console.Cli;
 using ThunderPipe.Settings;
 using ThunderPipe.Utils;
@@ -17,12 +18,32 @@ internal sealed class ValidateCommand : AsyncCommand<ValidateSettings>
 	{
 		var builder = RequestBuilder.Create(settings.Token, settings.Repository!);
 
-		await ThunderstoreAPI.ValidateIcon(
-			"/home/warpersan/Projects/TypeScript/action-thunderstore-check/__tests__/assets/icons/wrong-type.png",
-			//Path.Combine(settings.PackageFolder, "icon.png"),
-			builder.Copy(),
-			cancellationToken
-		);
+		var iconPath = Path.GetFullPath(settings.IconPath!, settings.PackageFolder);
+
+		var iconErrors = await ThunderstoreAPI.ValidateIcon(iconPath, builder, cancellationToken);
+
+		if (iconErrors == null)
+		{
+			Log.Error("Failed to validate the icon remotely.");
+			return 1;
+		}
+
+		if (iconErrors.FieldErrors != null || iconErrors.NonFieldErrors != null)
+		{
+			var errors = new List<string>();
+
+			if (iconErrors.FieldErrors != null)
+				errors.AddRange(iconErrors.FieldErrors);
+
+			if (iconErrors.NonFieldErrors != null)
+				errors.AddRange(iconErrors.NonFieldErrors);
+
+			Log.Error(
+				$"Icon '{iconPath}' has resulted in these errors:\n\n- {string.Join("\n- ", errors).EscapeMarkup()}"
+			);
+			return 1;
+		}
+
 		return 0;
 	}
 }
