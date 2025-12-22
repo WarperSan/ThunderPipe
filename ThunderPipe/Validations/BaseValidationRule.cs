17f14b6008f2fe5b01196fec7421443f64d795e9
WarperSan <leumas.ecole@gmail.com>
2025-12-22 18:00:41 -0500

    c

--- ThunderPipe/Validations/BaseValidationRule.cs
+++ ThunderPipe/Validations/BaseValidationRule.cs
@@ -10,5 +10,8 @@ internal abstract class BaseValidationRule
 	/// <summary>
 	/// Checks if this rule is valid
 	/// </summary>
-	public abstract Task<string?> Validate(RequestBuilder builder, CancellationToken cancellationToken);
+	public abstract Task<string?> Validate(
+		RequestBuilder builder,
+		CancellationToken cancellationToken
+	);
 }
