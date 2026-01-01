fc0d37c5cc1e5d5d898c0b6970955b4ed3e6b3c8
WarperSan <leumas.ecole@gmail.com>
2026-01-01 18:02:54 -0500

    Added logger

--- ThunderPipe/Program.cs
+++ ThunderPipe/Program.cs
@@ -1,4 +1,6 @@
-﻿using Spectre.Console.Cli;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Spectre.Console.Cli;
 using ThunderPipe.Commands;
 
 namespace ThunderPipe;
@@ -7,7 +9,22 @@ internal static class Program
 {
 	public static int Main(string[] args)
 	{
-		var app = new CommandApp();
+		var services = new ServiceCollection();
+
+		services.AddLogging(builder =>
+		{
+			builder.AddConsole();
+
+#if DEBUG
+			builder.SetMinimumLevel(LogLevel.Debug);
+#else
+			builder.SetMinimumLevel(LogLevel.Information);
+#endif
+		});
+
+		var registrar = new Utils.TypeRegistrar(services);
+
+		var app = new CommandApp(registrar);
 
 		app.Configure(config =>
 		{
