50430161c076b0e66aebe1cd07f1c9d1c410fed0
warpersan <leumas.ecole@gmail.com>
2026-01-10 20:07:20 -0500

    Added DTOs for `community`

---
+++ ThunderPipe/DTOs/GetCommunityPageDTOs.cs
@@ -0,0 +1,44 @@
+using Newtonsoft.Json;
+using ThunderPipe.Utils;
+
+namespace ThunderPipe.DTOs;
+
+/// <summary>
+/// Model used as the response payload in <see cref="ThunderstoreAPI.GetCommunityPage"/>
+/// </summary>
+internal record GetCommunityPageResponse
+{
+	public record PaginationModel
+	{
+		/// <summary>
+		/// URL of the next page
+		/// </summary>
+		[JsonProperty("next_link")]
+		public string? NextPage { get; set; }
+
+		/// <summary>
+		/// URL of the previous page
+		/// </summary>
+		[JsonProperty("previous_link")]
+		public string? PreviousPage { get; set; }
+	}
+
+	public record PageItemModel
+	{
+		[JsonProperty("identifier")]
+		[JsonRequired]
+		public required string Slug { get; set; }
+
+		[JsonProperty("name")]
+		[JsonRequired]
+		public required string Name { get; set; }
+	}
+
+	[JsonProperty("pagination")]
+	[JsonRequired]
+	public required PaginationModel Pagination { get; set; }
+
+	[JsonProperty("results")]
+	[JsonRequired]
+	public required PageItemModel[] Items { get; set; }
+}
