d035e9eea8146a2b6e3921707c4f67cefa68df4d
warpersan <leumas.ecole@gmail.com>
2026-01-20 09:49:20 -0500

    Added basic tests for `RegexHelper`

---
+++ ThunderPipe.Tests/UnitTests/Utils/RegexHelperTests.cs
@@ -0,0 +1,27 @@
+using ThunderPipe.Utils;
+
+namespace ThunderPipe.Tests.UnitTests.Utils;
+
+public class RegexHelperTests
+{
+	public static IEnumerable<object[]> IsNameValidData =>
+		new List<object[]>
+		{
+			new object[] { "Belmont", true },
+			new object[] { "Starstorm2", true },
+			new object[] { "Cyto_Commando", true },
+			new object[] { "ExponentialItemStacks", true },
+			new object[] { "Some_Mod", true },
+			new object[] { "HAND_OVERCLOCKED!", false },
+			new object[] { "Supply-Drop", false },
+		};
+
+	[Theory]
+	[MemberData(nameof(IsNameValidData))]
+	public void IsNameValid(string name, bool expected)
+	{
+		var actual = RegexHelper.IsNameValid(name);
+
+		Assert.Equal(expected, actual);
+	}
+}
