3b5ac9378673f431d161b0bca8142982ee64834b
warpersan <leumas.ecole@gmail.com>
2026-01-21 17:28:46 -0500

    Added case when only some dependencies were found

--- ThunderPipe.Tests/UnitTests/Clients/DependencyApiClientTests.cs
+++ ThunderPipe.Tests/UnitTests/Clients/DependencyApiClientTests.cs
@@ -42,6 +42,45 @@ public class DependencyApiClientTests
 	}
 
 	[Fact]
+	public async Task GetMissing_WhenSomeFound_ShouldReturnRemaining()
+	{
+		// Arrange
+		const string URL = "http://localhost:5050";
+
+		const string SLUG_1 = "ItsEmul-PEAKValuables-1.2.0";
+		const string SLUG_2 = "AtomicStudio-BetterScoutReport-0.1.6";
+		const string SLUG_3 = "SavageCore-PEAK_BritishEnglish_Translation-0.2.1";
+
+		var mockHttp = new MockHttpMessageHandler();
+
+		mockHttp.Expect(URL + "/*").RespondJSON(new Response { IsActive = true });
+
+		mockHttp.Expect(URL + "/*").RespondJSON(new Response { IsActive = false });
+
+		mockHttp.Expect(URL + "/*").RespondJSON(new Response { IsActive = false });
+
+		var builder = new RequestBuilder().ToUri(new Uri(URL));
+
+		var client = new DependencyApiClient(
+			builder,
+			mockHttp.ToHttpClient(),
+			CancellationToken.None
+		);
+
+		// Act
+		var requested = new[] { SLUG_1, SLUG_2, SLUG_3 };
+		var missing = await client.GetMissing(requested);
+
+		// Assert
+		var expected = new[] { SLUG_2, SLUG_3 };
+
+		Assert.Equal(missing.Count, expected.Length);
+
+		foreach (var slug in expected)
+			Assert.True(missing.Contains(slug));
+	}
+
+	[Fact]
 	public async Task GetMissing_WhenEmpty_ShouldSkipRequest()
 	{
 		// Arrange
