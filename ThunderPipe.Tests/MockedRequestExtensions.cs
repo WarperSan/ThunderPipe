dbe0d27ae7630a922e5c84ff14d1b0d90f9a84c3
warpersan <leumas.ecole@gmail.com>
2026-01-20 19:47:54 -0500

    Added extension for `MockedRequest`

---
+++ ThunderPipe.Tests/MockedRequestExtensions.cs
@@ -0,0 +1,16 @@
+using Newtonsoft.Json;
+using RichardSzalay.MockHttp;
+
+namespace ThunderPipe.Tests;
+
+internal static class MockedRequestExtensions
+{
+	/// <summary>
+	/// Sets the response of the current <see cref="T:MockedRequest"/>, with an OK (200) status code and with the JSON payload
+	/// </summary>
+	public static MockedRequest RespondJSON(this MockedRequest source, object content)
+	{
+		var json = JsonConvert.SerializeObject(content);
+		return source.Respond("application/json", json);
+	}
+}
