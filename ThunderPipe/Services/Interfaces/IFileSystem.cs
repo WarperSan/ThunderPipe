83a9fa2210b2dbb14488db53c45b89b2e16b9930
warpersan <leumas.ecole@gmail.com>
2026-01-23 11:48:29 -0500

    Created interface for accessing the file system

---
+++ ThunderPipe/Services/Interfaces/IFileSystem.cs
@@ -0,0 +1,22 @@
+namespace ThunderPipe.Services.Interfaces;
+
+/// <summary>
+/// Interface representing any class that handles IO operations
+/// </summary>
+internal interface IFileSystem
+{
+	/// <summary>
+	/// Opens an existing file for reading
+	/// </summary>
+	public FileStream OpenRead(string path) => File.OpenRead(path);
+
+	/// <summary>
+	/// Asynchronously opens a binary file, reads the contents of the file into a byte array, and then closes the file
+	/// </summary>
+	Task<byte[]> ReadAllBytesAsync(string path, CancellationToken cancellationToken);
+
+	/// <summary>
+	/// Asynchronously creates a new file, writes the specified string to the file, and then closes the file. If the target file already exists, it is overwritten
+	/// </summary>
+	Task WriteAllTextAsync(string path, string? contents, CancellationToken cancellationToken);
+}
