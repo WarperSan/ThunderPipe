8ac90ce4955fd7f4a6eaccfce34d0535e121afb4
warpersan <leumas.ecole@gmail.com>
2026-01-25 18:01:42 -0500

    Added `public`

--- ThunderPipe/Services/Interfaces/IFileSystem.cs
+++ ThunderPipe/Services/Interfaces/IFileSystem.cs
@@ -13,10 +13,14 @@ internal interface IFileSystem
 	/// <summary>
 	/// Asynchronously opens a binary file, reads the contents of the file into a byte array, and then closes the file
 	/// </summary>
-	Task<byte[]> ReadAllBytesAsync(string path, CancellationToken cancellationToken);
+	public Task<byte[]> ReadAllBytesAsync(string path, CancellationToken cancellationToken);
 
 	/// <summary>
 	/// Asynchronously creates a new file, writes the specified string to the file, and then closes the file. If the target file already exists, it is overwritten
 	/// </summary>
-	Task WriteAllTextAsync(string path, string? contents, CancellationToken cancellationToken);
+	public Task WriteAllTextAsync(
+		string path,
+		string? contents,
+		CancellationToken cancellationToken
+	);
 }
