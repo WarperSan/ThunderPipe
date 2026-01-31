92dce88bc66e769a5de20b0880afec6389a4cef2
warpersan <leumas.ecole@gmail.com>
2026-01-31 14:37:15 -0500

    Added `--log-level`

--- ThunderPipe/Settings/BaseCommandSettings.cs
+++ ThunderPipe/Settings/BaseCommandSettings.cs
@@ -5,4 +5,14 @@ namespace ThunderPipe.Settings;
 /// <summary>
 /// Settings used by any command
 /// </summary>
-public abstract class BaseCommandSettings : CommandSettings { }
+public abstract class BaseCommandSettings : CommandSettings
+{
+	[CommandOption("--log-level")]
+	[Description("Minimum logging level")]
+#if DEBUG
+	[DefaultValue(LogLevel.Debug)]
+#else
+	[DefaultValue(LogLevel.Information)]
+#endif
+	public LogLevel LogLevel { get; init; }
+}
