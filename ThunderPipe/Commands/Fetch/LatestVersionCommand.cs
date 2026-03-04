6eebcb42781227c26c4b504c339320c0091f0ba4
WarperSan <leumas.ecole@gmail.com>
2026-03-04 10:53:50 -0500

    Removed `_logger`

--- ThunderPipe/Commands/Fetch/LatestVersionCommand.cs
+++ ThunderPipe/Commands/Fetch/LatestVersionCommand.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics.CodeAnalysis;
-using Microsoft.Extensions.Logging;
 using Spectre.Console.Cli;
 
 namespace ThunderPipe.Commands.Fetch;
@@ -7,13 +6,6 @@ namespace ThunderPipe.Commands.Fetch;
 [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
 internal sealed class LatestVersionCommand : AsyncCommand<Settings.Fetch.BaseSettings>
 {
-	private readonly ILogger<LatestVersionCommand> _logger;
-
-	public LatestVersionCommand(ILogger<LatestVersionCommand> logger)
-	{
-		_logger = logger;
-	}
-
 	/// <inheritdoc />
 	public override async Task<int> ExecuteAsync(
 		CommandContext context,
