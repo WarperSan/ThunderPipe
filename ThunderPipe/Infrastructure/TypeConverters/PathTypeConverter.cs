783150cc83f2f95f5d4010fab69e20d42338c4cd
WarperSan <leumas.ecole@gmail.com>
2026-03-05 13:05:13 -0500

    Added `PathTypeConverter`

---
+++ ThunderPipe/Infrastructure/TypeConverters/PathTypeConverter.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel;
+using System.Globalization;
+
+namespace ThunderPipe.Infrastructure.TypeConverters;
+
+/// <summary>
+/// Converts a <see cref="string"/> to a valid absolute path
+/// </summary>
+internal sealed class PathTypeConverter : TypeConverter
+{
+	/// <inheritdoc />
+	public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
+	{
+		return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+	}
+
+	/// <inheritdoc />
+	public override object? ConvertFrom(
+		ITypeDescriptorContext? context,
+		CultureInfo? culture,
+		object value
+	)
+	{
+		if (value is not string path)
+			return base.ConvertFrom(context, culture, value);
+
+		path = Environment.ExpandEnvironmentVariables(path);
+
+		if (path.StartsWith("~/"))
+		{
+			var userProfilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+			path = Path.Combine(userProfilePath, path[2..]);
+		}
+
+		return Path.GetFullPath(path);
+	}
+}
