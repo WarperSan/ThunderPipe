0072c1a9ca78b75a793130015b4b5ec4a7fa57c9
WarperSan <leumas.ecole@gmail.com>
2026-03-05 15:04:29 -0500

    PackageSettings

--- ThunderPipe/Settings/Publish/PackageSettings.cs
+++ ThunderPipe/Settings/Publish/PackageSettings.cs
@@ -3,6 +3,8 @@ using System.Diagnostics.CodeAnalysis;
 using Spectre.Console;
 using Spectre.Console.Cli;
 using ThunderPipe.Commands.Publish;
+using ThunderPipe.Infrastructure.TypeConverters;
+using ThunderPipe.Models.Internal;
 
 namespace ThunderPipe.Settings.Publish;
 
@@ -11,21 +13,24 @@ namespace ThunderPipe.Settings.Publish;
 /// </summary>
 [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
 [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
-public sealed class PackageSettings : BaseSettings
+internal sealed class PackageSettings : BasePublishSettings
 {
+	private const string CATEGORY_OPTION = "--category";
+
 	[CommandArgument(0, "<file>")]
 	[Description("Path to the '.zip' file to publish")]
+	[TypeConverter(typeof(PathTypeConverter))]
 	public required string File { get; init; }
 
 	[CommandArgument(1, "<team>")]
 	[Description("Team that will own the published package")]
-	public required string Team { get; init; }
+	public required PackageTeam Team { get; init; }
 
 	[CommandArgument(2, "<community>")]
 	[Description("Slug of the community to publish the package to")]
 	public required string Community { get; init; }
 
-	[CommandOption("--category <CATEGORY>")]
+	[CommandOption($"{CATEGORY_OPTION} <CATEGORY>")]
 	[Description("Category slug to label the package with")]
 	public string[]? Categories { get; init; }
 
@@ -40,14 +45,24 @@ public sealed class PackageSettings : BaseSettings
 		if (!System.IO.File.Exists(File))
 			return ValidationResult.Error($"No file was found at '{File}'.");
 
-		if (string.IsNullOrWhiteSpace(Team))
-			return ValidationResult.Error("Team cannot be empty.");
+		if (!Team.IsValid())
+			return ValidationResult.Error($"'{Team}' is not a valid package team.");
 
 		if (string.IsNullOrWhiteSpace(Community))
 			return ValidationResult.Error("Community cannot be empty.");
 
-		if (Categories != null && Categories.Any(string.IsNullOrEmpty))
-			return ValidationResult.Error("Categories contains an empty item.");
+		if (Categories != null)
+		{
+			var invalidCategories = Categories.Where(string.IsNullOrWhiteSpace).ToArray();
+
+			if (invalidCategories.Length > 0)
+			{
+				var list = string.Join(", ", invalidCategories.Select(d => $"'{d}'"));
+				return ValidationResult.Error(
+					$"'{CATEGORY_OPTION}' contains invalid value(s): {list}"
+				);
+			}
+		}
 
 		return base.Validate();
 	}
