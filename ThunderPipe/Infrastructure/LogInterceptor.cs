f56a545879cf60daef026ec40913a425320ffc33
WarperSan <leumas.ecole@gmail.com>
2026-03-13 13:28:05 -0400

    Fixed incoherent `LogInterceptor.Level`

--- ThunderPipe/Infrastructure/LogInterceptor.cs
+++ ThunderPipe/Infrastructure/LogInterceptor.cs
@@ -6,10 +6,16 @@ namespace ThunderPipe.Infrastructure;
 
 internal sealed class LogInterceptor : ICommandInterceptor
 {
+#if DEBUG
+	public const LogLevel MINIMUM_LEVEL = LogLevel.Debug;
+#else
+	public const LogLevel MINIMUM_LEVEL = LogLevel.Information;
+#endif
+
 	/// <summary>
 	/// Current minimum log level
 	/// </summary>
-	public static LogLevel Level { get; private set; } = LogLevel.None;
+	public static LogLevel Level { get; private set; } = MINIMUM_LEVEL;
 
 	/// <inheritdoc/>
 	public void Intercept(CommandContext context, CommandSettings settings)
--- ThunderPipe/Settings/BaseCommandSettings.cs
+++ ThunderPipe/Settings/BaseCommandSettings.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel;
 using Microsoft.Extensions.Logging;
 using Spectre.Console.Cli;
+using ThunderPipe.Infrastructure;
 
 namespace ThunderPipe.Settings;
 
@@ -11,10 +12,6 @@ internal abstract class BaseCommandSettings : CommandSettings
 {
 	[CommandOption("--log-level")]
 	[Description("Minimum level of messages to display")]
-#if DEBUG
-	[DefaultValue(LogLevel.Debug)]
-#else
-	[DefaultValue(LogLevel.Information)]
-#endif
+	[DefaultValue(LogInterceptor.MINIMUM_LEVEL)]
 	public LogLevel LogLevel { get; init; }
 }
