a5d1eada838da2ea47c1af15315a1416c1883f98
WarperSan <leumas.ecole@gmail.com>
2026-03-14 22:40:54 -0400

    Use service

--- ThunderPipe/Commands/Publish/PackageCommand.cs
+++ ThunderPipe/Commands/Publish/PackageCommand.cs
@@ -1,8 +1,8 @@
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.Logging;
 using Spectre.Console.Cli;
-using ThunderPipe.Core.Clients;
-using ThunderPipe.Core.Models.Web.MultipartUpload;
+using ThunderPipe.Core.Models.API;
+using ThunderPipe.Core.Services.Implementations;
 using ThunderPipe.Core.Services.Interfaces;
 using ThunderPipe.Core.Utils;
 
@@ -30,100 +30,36 @@ internal sealed class PackageCommand : BaseCommand<Settings.Publish.PackageSetti
 
 		Logger.LogInformation("Starting to publish '{File}'.", file);
 
-		var builder = new RequestBuilder().ToUri(settings.Host!).WithAuth(settings.Token);
+		var builder = new RequestBuilder().ToUri(settings.Host!);
 
 		Logger.LogInformation("Publishing '{File}'", file);
 
-		using var client = new PublishApiClient();
-		client.Builder = builder;
-		client.Logger = Logger;
+		var service = new PublicationService(builder, _fileSystem, Logger);
 
-		var uploadSession = await client.InitiateMultipartUpload(
+		var package = await service.PublishPackage(
 			file,
-			_fileSystem,
-			cancellationToken
-		);
-
-		Logger.LogInformation(
-			"Uploading '{File}' ({GetSizeString}) in {ChunkCount} chunks.",
-			file,
-			GetSizeString(_fileSystem.GetSize(file)),
-			uploadSession.Parts.Count
-		);
-
-		IReadOnlyCollection<UploadPart> uploadedParts;
-
-		try
-		{
-			uploadedParts = await client.UploadParts(
-				file,
-				uploadSession.Parts,
-				_fileSystem,
-				cancellationToken
-			);
-		}
-		catch (Exception)
-		{
-			await client.AbortMultipartUpload(uploadSession.UUID, cancellationToken);
-			throw;
-		}
-
-		if (uploadedParts.Count != uploadSession.Parts.Count)
-		{
-			await client.AbortMultipartUpload(uploadSession.UUID, cancellationToken);
-			throw new InvalidOperationException("Failed to upload every parts.");
-		}
-
-		var finishedUpload = await client.FinishMultipartUpload(
-			uploadSession.UUID,
-			uploadedParts,
-			cancellationToken
-		);
-
-		if (!finishedUpload)
-			throw new InvalidOperationException("Failed to finish upload.");
-
-		Logger.LogInformation("Successfully finalized the upload.");
-
-		var releasedPackage = await client.SubmitPackage(
 			settings.Team,
-			settings.Community,
-			settings.Categories ?? [],
+			[settings.Community],
+			new Dictionary<Community, IEnumerable<Category>>
+			{
+				[settings.Community] = settings.Categories ?? [],
+			},
 			settings.HasNsfw,
-			uploadSession.UUID,
+			settings.Token,
 			cancellationToken
 		);
 
 		Logger.LogInformation(
 			"Successfully published '{VersionName}' v{VersionVersion}",
-			releasedPackage.Name,
-			releasedPackage.Version
+			package.Name,
+			package.Version
 		);
 
 		Logger.LogInformation(
 			"The package is now available at '{VersionDownloadURL}'.",
-			releasedPackage.DownloadURL
+			package.DownloadURL
 		);
 
 		return 0;
-	}
-
-	/// <summary>
-	/// Formats a byte size into readable text
-	/// </summary>
-	private static string GetSizeString(long byteSize)
-	{
-		double finalSize = byteSize;
-
-		string[] suffixes = ["B", "KB", "MB", "GB", "TB"];
-		var suffixIndex = 0;
-
-		while (finalSize >= 1024 && suffixIndex < suffixes.Length)
-		{
-			finalSize /= 1024;
-			suffixIndex++;
-		}
-
-		return $"{finalSize:F2} {suffixes[suffixIndex]}";
 	}
 }
