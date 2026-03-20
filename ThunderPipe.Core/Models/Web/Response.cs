8fc41ac065a2b3c55a0150fcd00281d03abcdb9f
WarperSan <leumas.ecole@gmail.com>
2026-03-20 11:16:15 -0400

    Created empty class

---
+++ ThunderPipe.Core/Models/Web/Response.cs
@@ -0,0 +1,7 @@
+namespace ThunderPipe.Core.Models.Web;
+
+/// <summary>
+/// Wrapper to handle <see cref="HttpResponseMessage"/> better
+/// </summary>
+internal sealed class Response<T>
+	where T : class { }
