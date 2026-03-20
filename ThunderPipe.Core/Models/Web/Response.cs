c2265be8e80004255e450a222f38ae3b62c3bb9f
WarperSan <leumas.ecole@gmail.com>
2026-03-20 11:19:04 -0400

    Added private constructor

--- ThunderPipe.Core/Models/Web/Response.cs
+++ ThunderPipe.Core/Models/Web/Response.cs
@@ -6,8 +6,18 @@ namespace ThunderPipe.Core.Models.Web;
 internal sealed class Response<T>
 	where T : class
 {
+	private Response() { }
+
 	/// <summary>
 	/// JSON content of the response if success
 	/// </summary>
 	public T? Data { get; private set; }
+
+	/// <summary>
+	/// Creates a new instance of <see cref="Response{T}"/> from the given response
+	/// </summary>
+	public static async Task<Response<T>> CreateResponse(
+		HttpResponseMessage response,
+		CancellationToken ct
+	) { }
 }
