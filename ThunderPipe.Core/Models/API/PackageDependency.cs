5f992830ee432de44e063dd0c809b686bdef9737
WarperSan <leumas.ecole@gmail.com>
2026-03-23 14:09:05 -0400

    Removed nullables

--- ThunderPipe.Core/Models/API/PackageDependency.cs
+++ ThunderPipe.Core/Models/API/PackageDependency.cs
@@ -21,38 +21,26 @@ public sealed record PackageDependency
 
 		if (components.Length == 3)
 		{
-			Team = new Team(components[0]);
+			Team = components[0];
 			Name = components[1];
 			Version = components[2];
 		}
 		else
 		{
-			Team = null;
-			Name = null;
-			Version = null;
+			Team = string.Empty;
+			Name = string.Empty;
+			Version = string.Empty;
 		}
 	}
 
-	public Team? Team { get; }
-	public PackageName? Name { get; }
-	public PackageVersion? Version { get; }
+	public Team Team { get; }
+	public PackageName Name { get; }
+	public PackageVersion Version { get; }
 
 	/// <summary>
 	/// Checks if the underlying value is valid
 	/// </summary>
-	public bool IsValid()
-	{
-		if (Team == null)
-			return false;
-
-		if (Name == null)
-			return false;
-
-		if (Version == null)
-			return false;
-
-		return Team.IsValid() && Name.IsValid() && Version.IsValid();
-	}
+	public bool IsValid() => Team.IsValid() && Name.IsValid() && Version.IsValid();
 
 	/// <inheritdoc />
 	public override string ToString() => _dependencyString;
